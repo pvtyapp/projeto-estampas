2e44ecffd64266fc74145647877a46412b85eea3
pvtyapp <pedrobuenofralves@gmail.com>
2026-01-08 14:59:27 -0300

    feat: preview

--- print-wizard/app/work/page.tsx
+++ print-wizard/app/work/page.tsx
@@ -1,8 +1,5 @@
 'use client'
 
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
-
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { useSession } from '@/app/providers/SessionProvider'
