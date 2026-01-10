111b3234ffd8df3425219a349aef960fde3027bd
pvtyapp <pedrobuenofralves@gmail.com>
2026-01-09 21:06:19 -0300

    feat: preview

--- print-wizard/app/work/Library.tsx
+++ print-wizard/app/work/Library.tsx
@@ -187,8 +187,7 @@ export default function Library({ onPreview, version }: Props) {
   }
 
   return (
-    <div className="mx-auto min-h-[280px] rounded-2xl border p-6 space-y-4 flex flex-col">
-
+    <div className="space-y-4 h-full min-h-[720px] flex flex-col">
       {toast && (
         <div className="fixed top-4 right-4 bg-black text-white px-4 py-2 rounded shadow z-50">
           {toast}
